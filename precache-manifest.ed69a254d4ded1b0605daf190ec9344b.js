2cb88609cbbd3489888c3708c1c12166db35210c
karthickeyan ganesan <karthic200.com>
2018-11-07 07:42:31 +0530

    Updates

---
+++ precache-manifest.ed69a254d4ded1b0605daf190ec9344b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-opencart-ecomm/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "351d3980927fbb5cd02a",
+    "url": "/react-opencart-ecomm/static/js/runtime~main.351d3980.js"
+  },
+  {
+    "revision": "12767777e9c04f782c6a",
+    "url": "/react-opencart-ecomm/static/js/main.12767777.chunk.js"
+  },
+  {
+    "revision": "5b966dc2e69f15274453",
+    "url": "/react-opencart-ecomm/static/js/1.5b966dc2.chunk.js"
+  },
+  {
+    "revision": "12767777e9c04f782c6a",
+    "url": "/react-opencart-ecomm/static/css/main.ab0f190c.chunk.css"
+  },
+  {
+    "revision": "b9118088bd71380169ca2c5d738e70d2",
+    "url": "/react-opencart-ecomm/index.html"
+  }
+];
